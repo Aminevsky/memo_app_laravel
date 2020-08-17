873f671c341aedf5436ada0af2674b76f0471421
aminevsky <amigomr@gmail.com>
2020-08-18 01:32:18 +0900

    Add Memo model and migration

---
+++ database/migrations/2020_08_18_004001_create_memos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable(false);
+            $table->text('body')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
