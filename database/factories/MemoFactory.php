f7c39dc6e188a83d6f1dbd35777b7269ff08e3ee
aminevsky <amigomr@gmail.com>
2020-09-01 14:51:52 +0900

    Add MemoFactory

---
+++ database/factories/MemoFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Memo;
+use Faker\Generator as Faker;
+
+$factory->define(Memo::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(255),
+        'body'  => $faker->text(5000),
+    ];
+});
